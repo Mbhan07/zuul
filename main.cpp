16b076b51b4197dec1899a32415f3a8ff3680148
U-BSD\419503 <419503@SHS-C5W2NW3.beaverton.k12.or.us>
2024-11-18 10:11:35 -0800

    Don't check the comments

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+/* This is the main class for Zuul, a text-based adventure game. The goal is to tavel through rooms and pick up items, eventually picking up a key and making our way out. My game is inspired on fast food locations as I am really hungry right now.
+
+   By: Mahika Bhan
+   11 / 14 / 2024
+
+   ( ͡👁️ ͜ʖ ͡👁️)
+   ( ͡👁️ ͜ʖ ͡👁️)
+   ¯\_( ͡❛ ͜ʖ ͡❛)_/¯ ¯\_( ͡❛ ͜ʖ ͡❛)_/¯ ¯\_( ͡❛ ͜ʖ ͡❛)_/¯ ¯\_( ͡❛ ͜ʖ ͡❛)_/¯ ¯\_( ͡❛ ͜ʖ ͡❛)_/¯
+   ツ유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유유
+*/
+
+int main(){
+  // i will need three vectors: one for the room, one for the items, and one for the user's inventory
+
+  /* it shoulook something like this, warranted that i create rooms.cpp, rooms.h, items.cpp, item.h, inventory.cpp, and inentory.h files 
+  vector <rooms*> rooms;
+  vector <items*> items;
+  vector <inventory*> inventory;
+  */
+  cout << "Welcome to Zuul!" <<endl;
+  cout << endl;
+  cout << "Currently, you are in the middle of Washington Square Mall." << endl;\
+  cout << endl;
+  cout << "Your objective is to pick up your dinner and make a quick exit to go eat it while it is still warm." << endl;
+  cout << endl;
+  cout << "Good luck!" << endl;
+  cout << endl;
+  return 0;
+
+}
+
+
