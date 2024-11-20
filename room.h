413eb3a42c8cef55bbce6694caaca1d534d7aa6c
Mahika Bhan <bhan.mahika813@gmail.com>
2024-11-19 22:05:34 -0800

    updating room.h:

--- room.h
+++ room.h
@@ -1,1 +1,19 @@
-//room.h file
+//header guard
+
+#ifndef ROOM
+#define ROOM
+
+#include <iostream>
+#include <cstring>
+#include <map>
+#include "item.h"
+#include <vector>
+
+using namespace std;
+
+/* woohoo! you've made it. i am still the same person. this file intializes all the needed functions for room.cpp to work
+
+   functions of interest: moving between rooms, adding items to rooms, dropping items in different rooms
+*/
+
+
